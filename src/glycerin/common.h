e2ee72dcfa7cb681fa43d987d1a4da7d4ce02dc2
Andy Brown <adb1413@rit.edu>
2012-01-21 23:44:21 -0500

    Added build system and initial code

---
+++ src/glycerin/common.h
@@ -0,0 +1,35 @@
+/*
+ * common.h
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GLYCERIN_COMMON_H
+#define GLYCERIN_COMMON_H
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <list>
+#include <map>
+#include <vector>
+#ifdef __APPLE__
+#define GL3_PROTOTYPES
+#include <OpenGL/gl3.h>
+#else
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GL/glx.h>
+#endif
+
+/**
+ * @defgroup geometry
+ * @defgroup image
+ * @defgroup math
+ * @defgroup shader
+ * @defgroup system
+ * @defgroup texture
+ * @defgroup transform
+ */
+
+#endif
---
+++ src/glycerin/system/ErrorChecker.hpp
@@ -0,0 +1,32 @@
+/*
+ * ErrorChecker.hpp
+ * 
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef GLYCERIN_ERRORCHECKER_HPP
+#define GLYCERIN_ERRORCHECKER_HPP
+#include "glycerin/common.h"
+namespace Glycerin {
+
+
+/**
+ * Utility for checking for OpenGL errors.
+ *
+ * @ingroup system
+ */
+class ErrorChecker {
+public:
+    static void checkErrors(const std::string &location);
+    static void clearErrors();
+private:
+    static void report(GLenum error, const std::string &location);
+    static std::string toString(GLenum error);
+// Constructors
+    ErrorChecker();
+    ErrorChecker(const ErrorChecker&);
+    ErrorChecker& operator=(const ErrorChecker&);
+};
+
+} /* namespace Glycerin */
+#endif
