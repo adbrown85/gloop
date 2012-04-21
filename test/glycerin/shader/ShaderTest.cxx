4336b9249d2e6a9ae05db8d07848fe3238c36ea6
Andy Brown <adb1413@rit.edu>
2012-04-20 21:52:22 -0400

    More test cases for Shader (closes #14)

--- test/glycerin/shader/ShaderTest.cxx
+++ test/glycerin/shader/ShaderTest.cxx
@@ -20,6 +20,74 @@ class ShaderTest {
 public:
 
     /**
+     * Ensures a bad fragment shader does not compile correctly.
+     */
+    void testCompileWithBadFragmentShader() {
+        Shader shader = Shader::create(GL_FRAGMENT_SHADER);
+        shader.source(
+                "#version 140\n"
+                "out vec4 FragColor;\n"
+                "void main() {\n"
+                "    gl_Position = vec4(1);\n"
+                "}\n");
+        shader.compile();
+        if (shader.compiled()) {
+            throw new runtime_error("Compiled bad fragment shader!");
+        }
+    }
+
+    /**
+     * Ensures a good fragment shader compiles correctly.
+     */
+    void testCompileWithGoodFragmentShader() {
+        Shader shader = Shader::create(GL_FRAGMENT_SHADER);
+        shader.source(
+                "#version 140\n"
+                "out vec4 FragColor;\n"
+                "void main() {\n"
+                "    FragColor = vec4(1);\n"
+                "}\n");
+        shader.compile();
+        if (!shader.compiled()) {
+            throw new runtime_error("Could not compile good fragment shader!");
+        }
+    }
+
+    /**
+     * Ensures a bad vertex shader does not compile correctly.
+     */
+    void testCompileWithBadVertexShader() {
+        Shader shader = Shader::create(GL_VERTEX_SHADER);
+        shader.source(
+                "#version 140\n"
+                "in vec4 MCVertex;\n"
+                "void main() {\n"
+                "    gl_FragColor = MCVertex;\n"
+                "}\n");
+        shader.compile();
+        if (shader.compiled()) {
+            throw new runtime_error("Compiled bad vertex shader!");
+        }
+    }
+
+    /**
+     * Ensures a good vertex shader compiles correctly.
+     */
+    void testCompileWithGoodVertexShader() {
+        Shader shader = Shader::create(GL_VERTEX_SHADER);
+        shader.source(
+                "#version 140\n"
+                "in vec4 MCVertex;\n"
+                "void main() {\n"
+                "    gl_Position = MCVertex;\n"
+                "}\n");
+        shader.compile();
+        if (!shader.compiled()) {
+            throw new runtime_error("Could not compile good vertex shader!");
+        }
+    }
+
+    /**
      * Ensures making a new fragment shader works.
      */
     void testCreateWithFragmentShader() {
@@ -50,6 +118,94 @@ public:
             throw new runtime_error("Could not create vertex shader!");
         }
         shader.dispose();
+    }
+
+    /**
+     * Ensures handle returns a valid handle with a fragment shader.
+     */
+    void testHandleWithFragmentShader() {
+        Shader shader = Shader::create(GL_FRAGMENT_SHADER);
+        assert (shader.handle() > 0);
+    }
+
+    /**
+     * Ensures handle returns a valid handle with a geometry shader.
+     */
+    void testHandleWithGeometryShader() {
+        Shader shader = Shader::create(GL_GEOMETRY_SHADER);
+        assert (shader.handle() > 0);
+    }
+
+    /**
+     * Ensures handle returns a valid handle with a vertex shader.
+     */
+    void testHandleWithVertexShader() {
+        Shader shader = Shader::create(GL_VERTEX_SHADER);
+        assert (shader.handle() > 0);
+    }
+
+    /**
+     * Ensures compiling a bad fragment shader generates a log.
+     */
+    void testLogWithBadFragmentShader() {
+        Shader shader = Shader::create(GL_FRAGMENT_SHADER);
+        shader.source(
+                "#version 140\n"
+                "out vec4 FragColor;\n"
+                "void main() {\n"
+                "    gl_Position = vec4(1);\n"
+                "}\n");
+        shader.compile();
+        const string log = shader.log();
+        assert (!log.empty());
+    }
+
+    /**
+     * Ensures compiling a good fragment shader generates an empty log.
+     */
+    void testLogWithGoodFragmentShader() {
+        Shader shader = Shader::create(GL_FRAGMENT_SHADER);
+        shader.source(
+                "#version 140\n"
+                "out vec4 FragColor;\n"
+                "void main() {\n"
+                "    FragColor = vec4(1);\n"
+                "}\n");
+        shader.compile();
+        const string log = shader.log();
+        assert (log.empty());
+    }
+
+    /**
+     * Ensures compiling a bad vertex shader generates a log.
+     */
+    void testLogWithBadVertexShader() {
+        Shader shader = Shader::create(GL_VERTEX_SHADER);
+        shader.source(
+                "#version 140\n"
+                "in vec4 MCVertex;\n"
+                "void main() {\n"
+                "    FragColor = MCVertex;\n"
+                "}\n");
+        shader.compile();
+        const string log = shader.log();
+        assert (!log.empty());
+    }
+
+    /**
+     * Ensures compiling a good vertex shader generates an empty log.
+     */
+    void testLogWithGoodVertexShader() {
+        Shader shader = Shader::create(GL_VERTEX_SHADER);
+        shader.source(
+                "#version 140\n"
+                "out vec4 MCVertex;\n"
+                "void main() {\n"
+                "    gl_Position = MCVertex;\n"
+                "}\n");
+        shader.compile();
+        const string log = shader.log();
+        assert (log.empty());
     }
 
     /**
@@ -90,6 +246,15 @@ public:
         Shader shader = Shader::create(GL_VERTEX_SHADER);
         assert(shader.type() == GL_VERTEX_SHADER);
     }
+
+    /**
+     * Ensures wrapping an existing shader works correctly.
+     */
+    void testWrap() {
+        const GLuint handle = glCreateShader(GL_FRAGMENT_SHADER);
+        Shader shader = Shader::wrap(handle);
+        assert (shader.handle() == handle);
+    }
 };
 
 
@@ -110,13 +275,25 @@ int main(int argc, char* argv[]) {
     // Run the test
     ShaderTest test;
     try {
+        test.testCompileWithBadFragmentShader();
+        test.testCompileWithGoodFragmentShader();
+        test.testCompileWithBadVertexShader();
+        test.testCompileWithGoodVertexShader();
         test.testCreateWithFragmentShader();
         test.testCreateWithGeometryShader();
         test.testCreateWithVertexShader();
+        test.testHandleWithFragmentShader();
+        test.testHandleWithGeometryShader();
+        test.testHandleWithVertexShader();
+        test.testLogWithBadFragmentShader();
+        test.testLogWithGoodFragmentShader();
+        test.testLogWithBadVertexShader();
+        test.testLogWithGoodVertexShader();
+        test.testSource();
         test.testTypeWithFragmentShader();
         test.testTypeWithGeometryShader();
         test.testTypeWithVertexShader();
-        test.testSource();
+        test.testWrap();
     } catch (exception& e) {
         cerr << e.what() << endl;
         throw e;
