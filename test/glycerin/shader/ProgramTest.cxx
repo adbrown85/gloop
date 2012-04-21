ada85ba5a1ea4fe96750424854799372c0caccda
Andy Brown <adb1413@rit.edu>
2012-04-21 15:22:18 -0400

    Program test case with bad shaders (ref #15)

--- test/glycerin/shader/ProgramTest.cxx
+++ test/glycerin/shader/ProgramTest.cxx
@@ -17,7 +17,21 @@ using namespace Glycerin;
 namespace bfs = boost::filesystem;
 
 
-const char* VERTEX_SHADER =
+const char* BAD_VERTEX_SHADER =
+        "#version 140\n"
+        "in vec4 MCVertex;\n"
+        "void main() {\n"
+        "    gl_position = MCVertex;\n"
+        "}\n";
+
+const char* BAD_FRAGMENT_SHADER =
+        "#version 140\n"
+        "out vec4 FragColor;\n"
+        "void main() {\n"
+        "    fragColor = vec4(1);\n"
+        "}\n";
+
+const char* GOOD_VERTEX_SHADER =
         "#version 140\n"
         "uniform mat4 MVPMatrix = mat4(1);\n"
         "in vec4 MCVertex;\n"
@@ -25,7 +39,7 @@ const char* VERTEX_SHADER =
         "    gl_Position = MVPMatrix * MCVertex;\n"
         "}\n";
 
-const char* FRAGMENT_SHADER =
+const char* GOOD_FRAGMENT_SHADER =
         "#version 140\n"
         "uniform vec4 Color = vec4(1);\n"
         "out vec4 FragColor;\n"
@@ -42,7 +56,7 @@ public:
     /**
      * Ensures Program links with a vertex and fragment shader.
      */
-    void testLinkWithVertexAndFragmentShader() {
+    void testLinkWithGoodVertexAndFragmentShader() {
 
         // Create a program
         Program program = Program::create();
@@ -50,10 +64,10 @@ public:
 
         // Create shaders
         Shader vs = Shader::create(GL_VERTEX_SHADER);
-        vs.source(VERTEX_SHADER);
+        vs.source(GOOD_VERTEX_SHADER);
         vs.compile();
         Shader fs = Shader::create(GL_FRAGMENT_SHADER);
-        fs.source(FRAGMENT_SHADER);
+        fs.source(GOOD_FRAGMENT_SHADER);
         fs.compile();
 
         // Add the shaders to the program
@@ -76,6 +90,41 @@ public:
         if (!program.valid()) {
             cerr << program.log() << endl;
             throw runtime_error("Could not validate program!");
+        }
+
+        // Delete it
+        program.dispose();
+    }
+
+    /**
+     * Ensures Program does not link and validate with a bad vertex and fragment shader.
+     */
+    void testLinkWithBadVertexAndFragmentShader() {
+
+        // Create a program
+        Program program = Program::create();
+        assert (program.handle() > 0);
+
+        // Create shaders
+        Shader vs = Shader::create(GL_VERTEX_SHADER);
+        vs.source(BAD_VERTEX_SHADER);
+        vs.compile();
+        Shader fs = Shader::create(GL_FRAGMENT_SHADER);
+        fs.source(BAD_FRAGMENT_SHADER);
+        fs.compile();
+
+        // Add the shaders to the program
+        program.attachShader(vs);
+        program.attachShader(fs);
+
+        // Delete the shaders
+        vs.dispose();
+        fs.dispose();
+
+        // Link the program
+        program.link();
+        if (program.linked()) {
+            throw runtime_error("Linked bad program!");
         }
 
         // Delete it
@@ -106,7 +155,8 @@ int main(int argc, char *argv[]) {
     // Run the test
     ProgramTest test;
     try {
-        test.testLinkWithVertexAndFragmentShader();
+        test.testLinkWithGoodVertexAndFragmentShader();
+        test.testLinkWithBadVertexAndFragmentShader();
     } catch (exception& e) {
         cerr << e.what() << endl;
         throw;
