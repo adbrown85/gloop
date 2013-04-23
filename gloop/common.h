42088f3800802a11ac5498cbadff083808e3a218
Andrew Brown <adb1413@rit.edu>
2013-04-22 21:36:34 -0400

    Only define GL3_PROTOTYPES and GL_GLEXT_PROTOTYPES if not already defined (ref #79)

--- gloop/common.h
+++ gloop/common.h
@@ -32,10 +32,14 @@
 #include <map>
 #include <vector>
 #ifdef __APPLE__
+#ifndef GL3_PROTOTYPES
 #define GL3_PROTOTYPES
+#endif
 #include <OpenGL/gl3.h>
 #else
+#ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES
+#endif
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include <GL/glx.h>
